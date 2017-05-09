82ad06ad86c249aa56f8218a2d769b8dcacd7060
Zachary Bedell <zbedell@bpcs.com>
2017-05-09 15:28:03 -0700

    Lists 3 and 4 of 4. Completed JSON wiring for index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,56 @@
 //Stairing is Caring scripts
 
+var topTeamsStairsFunc = function(){
+  $.getJSON('../data/top-team-list.json', function (topTeamsData){
+    console.log(topTeamsData);
+    topTeamsData.topTeams.map(function (topTeams){
+      var stairs = numeral(topTeams.totalStairs).format('(0,0)')
+      var content = '<li><img class="avatar-small pull-left" src="'
+      + topTeams.image
+      + '" /><a href="team/'
+      + topTeams.id
+      + '">'
+      + topTeams.name
+      + '</a><h3 class="text-muted">'
+      + stairs
+      + '</h3></li>';
+
+      $('#top-teams-stairs').append(content);
+    });
+  });
+};
+
+var topTeamsDonationsFunc = function(){
+  $.getJSON('../data/top-team-list.json', function (topTeamsData){
+    console.log(topTeamsData);
+    topTeamsData.topTeams.map(function (topTeams){
+      var dollars = numeral(topTeams.totalDonations).format('($0,0)')
+      var content = '<li><img class="avatar-small pull-left" src="'
+      + topTeams.image
+      + '" /><a href="team/'
+      + topTeams.id
+      + '">'
+      + topTeams.name
+      + '</a><h3 class="text-muted">'
+      + dollars
+      + '</h3></li>';
+
+      $('#top-teams-donations').append(content);
+    });
+  });
+};
+
 var climbingCompaniesFunc = function(){
-  $.getJSON('/data/top-company-list.json', function (climbingCompaniesData){
+  $.getJSON('../data/top-company-list.json', function (climbingCompaniesData){
     console.log(climbingCompaniesData);
-    var climbingCompaniesMap = climbingCompaniesData.topCompanies.map(function (topCompanies){
+    climbingCompaniesData.topCompanies.map(function (topCompanies){
+      var stairs = numeral(topCompanies.totalStairs).format('(0,0)')
       var content = '<li><img class="avatar-small pull-left" src="'
       + topCompanies.image
       + '" /><span>'
       + topCompanies.name
       + '</span><h3 class="text-muted">'
-      + topCompanies.totalStairs
+      + stairs
       + '</h3></li>';
 
       $('#company-stairs').append(content);
@@ -18,9 +59,9 @@ var climbingCompaniesFunc = function(){
 };
 
 var avgTeamDonationsFunc = function(){
-  $.getJSON('/data/top-company-list.json', function (avgTeamDonationsData){
+  $.getJSON('../data/top-company-list.json', function (avgTeamDonationsData){
     console.log(avgTeamDonationsData);
-    var avgTeamDonationsMap = avgTeamDonationsData.topCompanies.map(function (topCompanies){
+    avgTeamDonationsData.topCompanies.map(function (topCompanies){
       var dollars = numeral(topCompanies.avgTeamDonations).format('($0,0)')
       var content = '<li><p>'
       + topCompanies.name
@@ -36,10 +77,10 @@ var avgTeamDonationsFunc = function(){
 };
 
 var recentCommentFunc = function(){
-  $.getJSON('/data/recent-comments-data.json', function (recentCommentData){
+  $.getJSON('../data/recent-comments-data.json', function (recentCommentData){
     console.log(recentCommentData);
 
-    var recentCommentsMap = recentCommentData.recentComments.map(function (recentComments){
+    recentCommentData.recentComments.map(function (recentComments){
       var commentDatetime = moment(recentComments.created_at).format('h:m A MMM d YYYY');
       var content =
       '<div class="recent-activity"><img class="avatar-small pull-left" src="'
@@ -58,10 +99,10 @@ var recentCommentFunc = function(){
 };
 
 var eventCardFunc = function(){
-  $.getJSON('/data/event-card-data.json', function (eventCardData){
+  $.getJSON('../data/event-card-data.json', function (eventCardData){
     console.log(eventCardData);
 
-    var eventsData = eventCardData.events.map(function (events){
+    eventCardData.events.map(function (events){
       var eventDate = moment(events.event_date).format('MMM Do, YYYY');
       var content =
         '<div class="col-sm-3"><div class="event-card"><img src="'
@@ -79,39 +120,44 @@ var eventCardFunc = function(){
   });
 };
 
-var totalDollarsFunc = function(){
-  $.getJSON('/data/total-dollars.json', function (totalDollarsData){
-    console.log(totalDollarsData);
+var homeTotalsFunc = function(){
+  $.getJSON('../data/home-totals.json', function (homeTotalsData){
+    console.log(homeTotalsData);
 
-    totalDollarsData.totalDollars.map(function (totalDollars){
-      $('#total-dollars').html('Total Raised: ' + totalDollars.count);
+    homeTotalsData.homeTotals.map(function (homeTotals){
+      var content = '<li><h3>'
+      + homeTotals.count + ' ' + homeTotals.object
+      + '</h3></li><li><h3>';
+
+      $('#home-totals').append(content);
     });
 
+    homeTotalsData.totalDollars.map(function (totalDollars){
+       var dollars = numeral(totalDollars.count).format('($0,0)')
+       $('#total-dollars').html('Total Raised: ' + dollars);
+       $('#what-is-donation-total').html(dollars);
+     });
+
+     homeTotalsData.totalStairs.map(function (totalStairs){
+        var stairs = numeral(totalStairs.count).format('(0,0)')
+        $('#what-is-stairs-total').html(stairs);
+      });
+
+    homeTotalsData.totalCharities.map(function (totalCharities){
+       var charities = numeral(totalCharities.count).format('(0,0)')
+       $('#what-is-charity-count').html(charities);
+     });
 
   })
 };
 
-var homeTotalsFunc = function(){
-  $.getJSON('/data/home-totals.json', function (homeTotalsData){
-    console.log(homeTotalsData);
-
-    var items = homeTotalsData.homeTotals.map(function (homeTotals){
-      return homeTotals.count + ' ' + homeTotals.object;
-    });
-
-    if (items.length) {
-      var content = '<li><h3>' + items.join('</h3></li> <li><h3>') + '</li></h3>';
-      $('#home-totals').append(content);
-    }
-
-  });
-};
 
 $(document).ready(function(){
   homeTotalsFunc();
-  totalDollarsFunc();
   eventCardFunc();
   recentCommentFunc();
   avgTeamDonationsFunc();
   climbingCompaniesFunc();
+  topTeamsDonationsFunc();
+  topTeamsStairsFunc();
 });
